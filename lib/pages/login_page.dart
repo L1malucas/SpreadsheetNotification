7cddcac6a8238bc4794428b141bbf7fafe9a4694
Lucas Lima <lucaslima.94@hotmail.com.br>
2023-07-15 12:27:29 -0300

    firebase config

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spreadsheet_notifications/services/sign_in_request.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatefulWidget {
@@ -24,6 +25,7 @@ class _LoginPageState extends State<LoginPage> {
             children: [
               InkWell(
                 onTap: () {
+                  SignInRequest().signInGoogle();
                   print('object');
                 },
                 child: SizedBox(
---
+++ lib/services/sign_in_request.dart
@@ -0,0 +1,13 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignInRequest {
+  signInGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
